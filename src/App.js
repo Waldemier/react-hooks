ad884de941f6cdfc72d68676f09ff18c22cfcd3a
Wolodymyr <76656624+Waldemier@users.noreply.github.com>
2021-03-11 21:55:23 +0200

    init proj commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import './App.scss';
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container pt-4">
+        <h1>Hello React hooks!</h1>
+      </div>`
+    </ React.Fragment>
+  );
+}
+
+export default App;
